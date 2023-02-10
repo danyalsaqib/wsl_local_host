aae60b629c3e40b3d6c3d7ce08955df6ea5d2cf0
Danyal Saqib <dsaqib.bee17seecs@seecs.edu.pk>
2023-02-10 04:58:58 -0700

    Update get_localhost_wsl.py

--- get_localhost_wsl.py
+++ get_localhost_wsl.py
@@ -1,3 +1,5 @@
+import subprocess
+
 def get_localhost_wsl():
   result = subprocess.check_output("cat /etc/resolv.conf | grep nameserver | awk '{ print $2 }'", shell=True, text=True)
   result = result[:-1]
