518e1652adfec858764e378e00cd2fea15fc06e2
Danyal Saqib <dsaqib.bee17seecs@seecs.edu.pk>
2023-02-10 04:42:26 -0700

    Create get_localhost_wsl.py

---
+++ get_localhost_wsl.py
@@ -0,0 +1,6 @@
+def get_localhost_wsl():
+  result = subprocess.check_output("cat /etc/resolv.conf | grep nameserver | awk '{ print $2 }'", shell=True, text=True)
+  result = result[:-1]
+  print(result)
+
+  return result
